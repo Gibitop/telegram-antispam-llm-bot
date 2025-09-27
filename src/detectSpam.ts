fbea0e99bce8a73d7dd8a0c50f3df0bcc2f8cd07
Ilya Nikishin <ilya.ruwork@gmail.com>
2025-09-27 10:50:31 +0200

    v1.2.0

--- src/detectSpam.ts
+++ src/detectSpam.ts
@@ -3,7 +3,7 @@ import type { ChatCompletionMessageParam } from 'openai/resources/chat/completio
 import { env } from './env';
 
 
-const openrouter = new OpenAI({
+const openRouter = new OpenAI({
     baseURL: 'https://openrouter.ai/api/v1',
     apiKey: env.OPEN_ROUTER_TOKEN,
 });
@@ -14,7 +14,7 @@ export async function detectSpam(messageText: string) {
         { role: 'user', content: messageText }
     ];
 
-    const completion = await openrouter.chat.completions.create({
+    const completion = await openRouter.chat.completions.create({
         model: env.MODEL,
         messages,
     });
@@ -24,5 +24,8 @@ export async function detectSpam(messageText: string) {
     // LLMs often almost valid JSON or JSON in the middle of some grabage text
     // So we extract it with a regex instead of JSON.parse
     const match = content.match(/\{.*?\"?spam\"?\s*:\s*(true|false).*?\}/i);
-    return match?.[1]?.toLowerCase() === 'true'
+    return {
+        isSpam: match?.[1]?.toLowerCase() === 'true',
+        fullResponse: content,
+    }
 }
